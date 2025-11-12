c3e286c3988afc6f528c00318d890250a957db9f
Yor yani Mosquera Lopez <mosqueralopezyoryani@gmail.com>
2025-11-11 19:38:22 -0500

    cambio

--- iniciar_chocolimpio.bat
+++ iniciar_chocolimpio.bat
@@ -1,8 +1,9 @@
 @echo off
 echo INICIANDO CHOCÓ LIMPIO 2025...
-start "" "C:\xampp\xampp-control.exe"
-timeout /t 10
-pip install flask mysql-connector-python
-cd "C:\Users\mosqu\OneDrive\Desktop\pfe"
-python app.py
+echo.
+echo Activando entorno virtual y ejecutando la aplicacion...
+call "C:\Users\mosqu\OneDrive\Desktop\pfe\venv\Scripts\activate.bat"
+echo.
+echo Entorno activado. Iniciando servidor Flask...
+start "" python "C:\Users\mosqu\OneDrive\Desktop\pfe\app.py"
 pause
